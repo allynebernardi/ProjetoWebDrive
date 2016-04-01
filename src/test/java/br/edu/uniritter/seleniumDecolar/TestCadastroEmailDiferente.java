880fe659e5d7e8fb9fffd3ad8857f2a495a8ea97
unknown <alu201530615@ZS-LAB103-65025.uniritter.local>
2016-04-01 20:41:17 -0300

    Alteração Classe Test

--- src/test/java/br/edu/uniritter/seleniumDecolar/TestCadastroEmailDiferente.java
+++ src/test/java/br/edu/uniritter/seleniumDecolar/TestCadastroEmailDiferente.java
@@ -25,7 +25,7 @@ public class TestCadastroEmailDiferente {
 	@Test
 	public void testCadastroEmailDiferente() throws Exception {
 		driver.get(baseUrl + "/?gclid=CLjts8TyzMsCFYclgQodjdQPQA");
-		driver.findElement(By.id("lnkCadastroHeader")).click();
+		//driver.findElement(By.id("lnkCadastroHeader")).click();
 		driver.findElement(By.id("txtNomeCadastro")).click();
 		driver.findElement(By.id("txtNomeCadastro")).click();
 		driver.findElement(By.id("txtNomeCadastro")).click();
