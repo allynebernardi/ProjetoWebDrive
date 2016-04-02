c0fa45c54a774f0a220810f2bff7798fc82d56f2
unknown <alu201530615@ZS-LAB103-65025.uniritter.local>
2016-04-01 21:09:51 -0300

    Alteração teste5

--- src/test/java/br/edu/uniritter/seleniumDecolar/TestCadastroEmailDiferente.java
+++ src/test/java/br/edu/uniritter/seleniumDecolar/TestCadastroEmailDiferente.java
@@ -1,99 +1,48 @@
+
 package br.edu.uniritter.seleniumDecolar;
 
-import java.util.regex.Pattern;
+import static org.junit.Assert.fail;
+
 import java.util.concurrent.TimeUnit;
-import org.junit.*;
-import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
-import org.openqa.selenium.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
 
 public class TestCadastroEmailDiferente {
-  private WebDriver driver;
-  private String baseUrl;
-  private boolean acceptNextAlert = true;
-  private StringBuffer verificationErrors = new StringBuffer();
+private WebDriver driver;
+private String baseUrl;
+private StringBuffer verificationErrors = new StringBuffer();
 
-  @Before
-  public void setUp() throws Exception {
-    driver = new FirefoxDriver();
-    baseUrl = "http://www.zapimoveis.com.br/";
-    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
+@Before
+public void setUp() throws Exception {
+driver = new FirefoxDriver();
+baseUrl = "http://www.skoob.com.br/";
+driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+}
 
-  @Test
-  public void testCadastroEmailDiferente() throws Exception {	
-    driver.get(baseUrl + "/");
-  //  driver.findElement(By.id("lnkCadastroHeader")).click();
-    driver.findElement(By.id("txtNomeCadastro")).click();
-    driver.findElement(By.id("txtNomeCadastro")).click();
-    driver.findElement(By.id("txtNomeCadastro")).click();
-    driver.findElement(By.id("txtNomeCadastro")).clear();
-    driver.findElement(By.id("txtNomeCadastro")).sendKeys("Teste Selenium");
-    driver.findElement(By.id("txtEmailCadastro")).click();
-    driver.findElement(By.id("txtEmailCadastro")).clear();
-    driver.findElement(By.id("txtEmailCadastro")).sendKeys("allynbernardi@gmail.com");
-    driver.findElement(By.id("txtConfirmaEmailCadastro")).click();
-    driver.findElement(By.id("txtConfirmaEmailCadastro")).click();
-    driver.findElement(By.id("txtConfirmaEmailCadastro")).clear();
-    driver.findElement(By.id("txtConfirmaEmailCadastro")).sendKeys("allynebernardi@gmail.com");
-    driver.findElement(By.id("txtSenhaCadastro")).click();
-    driver.findElement(By.id("txtSenhaCadastro")).click();
-    driver.findElement(By.id("txtSenhaCadastro")).clear();
-    driver.findElement(By.id("txtSenhaCadastro")).sendKeys("ihu287f2@");
-    driver.findElement(By.id("txtConfirmaSenhaCadastro")).click();
-    driver.findElement(By.id("txtConfirmaSenhaCadastro")).clear();
-    driver.findElement(By.id("txtConfirmaSenhaCadastro")).sendKeys("ihu287f2@");
-    driver.findElement(By.cssSelector("label.clear")).click();
-    driver.findElement(By.id("chkNovidades")).click();
-    driver.findElement(By.id("btnConfirmarCadastro")).click();
-    driver.findElement(By.id("txtEmailCadastro")).click();
-    driver.findElement(By.id("txtEmailCadastro")).click();
-    driver.findElement(By.id("txtEmailCadastro")).click();
-    driver.findElement(By.id("txtEmailCadastro")).clear();
-    driver.findElement(By.id("txtEmailCadastro")).sendKeys("allynebernardi@gmail.com");
-  }
+@Test
+public void testCadastroSkoob() throws Exception {
+driver.get(baseUrl + "/");
+driver.findElement(By.linkText("Entrar")).click();
+driver.findElement(By.id("UsuarioEmail")).clear();
+driver.findElement(By.id("UsuarioEmail")).sendKeys("yalis.couto@gmail.com");
+driver.findElement(By.id("UsuarioSenha")).clear();
+driver.findElement(By.id("UsuarioSenha")).sendKeys("skoob123");
+driver.findElement(By.name("data[Login][automatico]")).click();
+driver.findElement(By.cssSelector("input[type=\"submit\"]")).click();
+}
 
-  @After
-  public void tearDown() throws Exception {
-    driver.quit();
-    String verificationErrorString = verificationErrors.toString();
-    if (!"".equals(verificationErrorString)) {
-      fail(verificationErrorString);
-    }
-  }
+@After
+public void tearDown() throws Exception {
+driver.quit();
+String verificationErrorString = verificationErrors.toString();
+if (!"".equals(verificationErrorString)) {
+fail(verificationErrorString);
+}
+}
 
-  private boolean isElementPresent(By by) {
-    try {
-      driver.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
-  }
-
-  private boolean isAlertPresent() {
-    try {
-      driver.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
-  }
-
-  private String closeAlertAndGetItsText() {
-    try {
-      Alert alert = driver.switchTo().alert();
-      String alertText = alert.getText();
-      if (acceptNextAlert) {
-        alert.accept();
-      } else {
-        alert.dismiss();
-      }
-      return alertText;
-    } finally {
-      acceptNextAlert = true;
-    }
-  }
 }
